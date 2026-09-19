4d1ffe8b2d386fc49a9c6b99a63f3d38c248016a
Paolo Anzani <paoloanzn@gmail.com>
2026-09-19 10:18:44 +0000

    fix: correct path to Lua configuration file

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ int main() {
 	lua_State *L = luaL_newstate();
 	luaL_openlibs(L);
 
-	if (luaL_loadfile(L, "configuration/config.lua") || lua_pcall(L, 0, 0, 0)) {
+	if (luaL_loadfile(L, "config.lua") || lua_pcall(L, 0, 0, 0)) {
 		printf("Some errors were found: %s\n",
 			lua_tostring(L, -1));
 			lua_close(L);
